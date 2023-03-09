8ab40ee9d269fc38bbf53c3ea6d4493ded54a76e
harsh.singh@borderfree.io-at-297767201074 <harsh.singh@borderfree.io>
2023-03-09 20:08:53 +0530

    removed CORD error

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -16,7 +17,7 @@ const jwtSecret = process.env.JWT_SECRET;
 // Connect to MongoDB
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected'))
+  .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
 // User Schema
@@ -26,34 +27,40 @@ const userSchema = new mongoose.Schema({
 });
 
 // User Model
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:3000",
+    origin: "http://localhost:3000",
+  })
+);
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Welcome to CRUD API');
+app.get("/", (req, res) => {
+  res.send("Welcome to CRUD API");
 });
 
 // User registration
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   try {
     const { email, password, confirmPassword } = req.body;
 
     if (!email || !password || !confirmPassword) {
-      return res.status(400).json({ message: 'Please fill all fields' });
+      return res.status(400).json({ message: "Please fill all fields" });
     }
 
     if (password !== confirmPassword) {
-      return res.status(400).json({ message: 'Passwords do not match' });
+      return res.status(400).json({ message: "Passwords do not match" });
     }
 
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: "User already exists" });
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -62,98 +69,99 @@ app.post('/register', async (req, res) => {
     const newUser = new User({ email, password: hashedPassword });
     await newUser.save();
 
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Something went wrong' });
+    res.status(500).json({ message: "Something went wrong" });
   }
 });
 
 // User login
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(400).json({ message: 'Please fill all fields' });
+      return res.status(400).json({ message: "Please fill all fields" });
     }
 
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({ message: 'Invalid email or password' });
+      return res.status(400).json({ message: "Invalid email or password" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid email or password' });
+      return res.status(400).json({ message: "Invalid email or password" });
     }
 
-    const token = jwt.sign({ userId: user._id }, jwtSecret, { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user._id }, jwtSecret, {
+      expiresIn: "1h",
+    });
 
     res.status(200).json({ token });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Something went wrong' });
+    res.status(500).json({ message: "Something went wrong" });
   }
 });
 
 // Get all users
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
     const users = await User.find({}, { password: 0 });
 
     res.status(200).json({ users });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Something went wrong' });
+    res.status(500).json({ message: "Something wenttt wrong" });
   }
 });
-app.get('/users/:id', async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id, { password: 0 });
-  
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      res.status(200).json({ user });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Something went wrong' });
-    }
-  });
-  app.put('/users/:id', async (req, res) => {
-    try {
-      const { email, password } = req.body;
-  
-      if (!email || !password) {
-        return res.status(400).json({ message: 'Please fill all fields' });
-      }
-  
-      const user = await User.findById(req.params.id);
-  
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-  
-      user.email = email;
-      user.password = hashedPassword;
-  
-      await user.save();
-  
-      res.status(200).json({ message: 'User updated successfully' });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Something went wrong' });
-    }
-  });
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id, { password: 0 });
 
-  app.listen(port,()=>{
-    console.log(`server is up at ${port} `)
-  })
-  
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+app.put("/users/:id", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user.email = email;
+    user.password = hashedPassword;
+
+    await user.save();
+
+    res.status(200).json({ message: "User updated successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server is up at ${port} `);
+});
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router,  Route, Routes } from 'react-router-dom';
-import axios from 'axios';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from "axios";
+import "./App.css";
 
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Profile from './components/Profile';
-
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Profile from "./components/Profile";
 
 function App() {
   const [users, setUsers] = useState([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios.get('/api/users')
-      .then(response => {
+    axios
+      .get("http://localhost:3001/users")
+      .then((response) => {
         setUsers(response.data);
       })
-      .catch(error => {
-        setError('Error fetching users. Please try again later.');
+      .catch((error) => {
+        setError("Error fetching users. Please try again later.");
       });
   }, []);
 
@@ -29,14 +29,18 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path="/" component={Login} />
+          <Route path="/" component={Login} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/profile/:id">
-          <Route exact path="/profile/:id" element={<Profile users={users} />} />
+            <Route
+              exact
+              path="/profile/:id"
+              component={<Profile users={users} />}
+            />
           </Route>
         </Routes>
       </Router>
     </div>
   );
 }
- export default App;
+export default App;
